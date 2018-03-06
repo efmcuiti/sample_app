1a050e1c012369d6ec532fa52999541bd2d91bd1
Edison Mendez <efmcuiti@gmail.com>
2018-03-06 11:56:23 -0500

    Finish static pages.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,6 +7,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Rails tutorial sample app"
   end
 
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+  
   test "should get home" do
     get static_pages_home_url
     assert_response :success
